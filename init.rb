71be0fb3bea164c7f2b509eeeaa4e1d827eb5db2
geemus <geemus@gmail.com>
2012-04-24 18:42:21 -0500

    fixes for relative paths

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require("heroku/client")
-require("./lib/ext/heroku/client")
+require("#{File.dirname(__FILE__)}/lib/ext/heroku/client")
 
 require("heroku/command/addons")
-require("./lib/ext/heroku/command/addons")
+require("#{File.dirname(__FILE__)}/lib/ext/heroku/command/addons")
