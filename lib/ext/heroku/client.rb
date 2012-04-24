c3742d33471043668622fb132d600e74c31da029
geemus <geemus@gmail.com>
2012-04-24 18:32:52 -0500

    initial implementations for info/index/add/remove

--- lib/ext/heroku/client.rb
+++ lib/ext/heroku/client.rb
@@ -13,4 +13,32 @@ class Heroku::Client
     json_decode(get("/addons/#{addon}"))
   end
 
+  # attachments
+
+  def add_attachment(app, resource, options = {})
+    json_decode(post("/apps/#{app}/attachments", options.merge(:resource_name => resource)))
+  end
+
+  def delete_attachment(app, config_var)
+    json_decode(delete("/apps/#{app}/attachments/#{config_var}"))
+  end
+
+  def get_attachments(app)
+    json_decode(get("/apps/#{app}/attachments"))
+  end
+
+  # resources
+
+  def add_resource(app, resource, options = {})
+    json_decode(post("/resources", options.merge(:app_name => app, :addon => resource)))
+  end
+
+  def delete_resource(resource)
+    json_decode(delete("/resources/#{resource}"))
+  end
+
+  def get_resource(resource)
+    json_decode(get("/resources/#{resource}"))
+  end
+
 end
--- lib/ext/heroku/command/addons.rb
+++ lib/ext/heroku/command/addons.rb
@@ -4,6 +4,109 @@ module Heroku::Command
   #
   class Addons < Base
 
+    # addons:add ADDON | NAME
+    #
+    # install an addon
+    #
+    #     --config CONFIG  # a non-default config var to use for attaching a resource
+    #
+    def add
+      argument = args.first
+      options = if config_var = extract_option('--config', nil)
+        { :config_var => config_var }
+      else
+        {}
+      end
+
+      if addon_info = heroku.addon(argument) rescue nil
+        if config_var
+          raise CommandFailed.new("--config is not a valid option for non-attachable addons")
+        end
+        if addon_info["attachable"] == false
+          # new non-attachable resource
+          configure_addon('Adding') do |addon, config|
+            heroku.install_addon(app, addon, config)
+          end
+        else
+          # new attachable resource
+          resource_info = nil
+          action("Adding #{argument} to #{app}") do
+            resource_info = heroku.add_resource(app, argument, options)
+          end
+          attachment = resource_info["attachments"].detect {|attachment| attachment["app"]["name"] == app}
+          display("#{resource_info["name"]} assigned to #{attachment["config_var"]}")
+        end
+      elsif resource_info = heroku.get_resource(argument) rescue nil
+        # existing attachable resource
+        attachment_info = nil
+        action("Adding #{argument} to #{app}") do
+          attachment_info = heroku.add_attachment(app, argument, options)
+        end
+        display("#{argument} assigned to #{attachment_info["config_var"]}")
+      else
+        display("Addon or resource not found")
+      end
+    end
+
+    # addons
+    #
+    # list installed addons
+    #
+    def index
+      installed = heroku.installed_addons(app)
+      attachments = heroku.get_attachments(app)
+
+      if attachments.empty? && installed.empty?
+        display "No addons installed"
+      else
+
+      resources = Hash.new {|hash,key| hash[key] = {}}
+
+      attachments.each do |attachment|
+        resource = attachment["resource"]["name"]
+        resources[resource]["Billing App"] = attachment["app"]["name"]
+        resources[resource]["Config"] ||= []
+        resources[resource]["Config"] << attachment["config_var"]
+        type = attachment["resource"]["type"]
+        resources[resource]["Type"] = type
+        installed.reject! {|addon| addon["name"] == type}
+      end
+      resources.map {|resource, data| data["Config"] = data["Config"].join(", ")}
+
+      resources.keys.sort.each do |resource_name|
+        display("=== #{resource_name}")
+        resource_data = resources[resource_name]
+        max_length = resource_data.keys.map {|key| key.length}.max + 2
+        resource_data.keys.sort.each do |key|
+          display("#{key}:".ljust(max_length), false)
+          display(resource_data[key])
+        end
+      end
+
+      available, pending = installed.partition { |a| a['configured'] }
+
+      unless available.empty?
+        display("=== Other Addons")
+        available.map do |a|
+            if a['attachment_name']
+              a['name'] + ' => ' + a['attachment_name']
+            else
+              a['name']
+            end
+          end.sort.each do |addon|
+            display(addon)
+          end
+        end
+
+        unless pending.empty?
+          display "\n--- not configured ---"
+          pending.map { |a| a['name'] }.sort.each do |addon|
+            display addon.ljust(24) + "http://#{heroku.host}/myapps/#{app}/addons/#{addon}"
+          end
+        end
+      end
+    end
+
     # addons:info ADDON
     #
     # list info for an addon
@@ -24,6 +127,52 @@ module Heroku::Command
         title_cased = key.split(" ").map {|word| word[0...1].upcase + word[1..-1]}.join(" ")
         display("#{title_cased}: ".ljust(max_length), false)
         display(addon_info[key])
+      end
+    end
+
+    # addons:remove ADDON | NAME
+    #
+    # uninstall an addon
+    #
+    def remove
+      return unless confirm_command
+      options[:confirm] ||= app
+
+      args.each do |name|
+        if resource_info = heroku.get_resource(name) rescue nil
+          if resource_info["attachments"].length > 0
+            attachments = resource_info["attachments"].select {|attachment| attachment["app"]["name"] == app}
+            if attachments.length > 1
+              config_var = attachments.first["config_var"]
+              action("Removing #{name} from #{app}") do
+                heroku.delete_attachment(app, config_var)
+              end
+              display("#{name} no longer assigned to #{config_var}")
+            else
+              attachment = resource_info["attachments"].first
+              action("Removing #{name} from #{app}") do
+                heroku.delete_resource(name)
+              end
+              display("#{name} no longer assigned to #{attachment["config_var"]}")
+            end
+          else
+            attachment = resource_info["attachments"].first
+            action("Removing #{name} from #{app}") do
+              heroku.delete_resource(name)
+            end
+            display("#{name} no longer assigned to #{attachment["config_var"]}")
+          end
+        elsif addon_info = heroku.addon(name) rescue nil
+          # non resource
+          messages = nil
+          action("Removing #{name} from #{app}") do
+            messages = addon_run { heroku.uninstall_addon(app, name, :confirm => options[:confirm]) }
+          end
+          output messages[:attachment] if messages[:attachment]
+          output messages[:message]
+        else
+          display("Addon or resource not found")
+        end
       end
     end
 
