940f4027fd20b4b596fd864e85966af90ca1bcc2
geemus <geemus@gmail.com>
2012-08-24 14:46:24 -0500

    remove auto-promotion after credentials --reset
    closes #13

--- init.rb
+++ init.rb
@@ -4,5 +4,8 @@ require("#{File.dirname(__FILE__)}/lib/ext/heroku/client")
 require("heroku/command/addons")
 require("#{File.dirname(__FILE__)}/lib/ext/heroku/command/addons")
 
+require("heroku/command/pg")
+require("#{File.dirname(__FILE__)}/lib/ext/heroku/command/pg")
+
 require("heroku/helpers/heroku_postgresql")
 require("#{File.dirname(__FILE__)}/lib/ext/heroku/helpers/heroku_postgresql")
