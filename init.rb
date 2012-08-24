46084b5f9ced6305a276b62b72f4a313cf2d2b67
geemus <geemus@gmail.com>
2012-08-23 23:31:51 -0500

    make pg commands work with attached things

--- init.rb
+++ init.rb
@@ -3,3 +3,6 @@ require("#{File.dirname(__FILE__)}/lib/ext/heroku/client")
 
 require("heroku/command/addons")
 require("#{File.dirname(__FILE__)}/lib/ext/heroku/command/addons")
+
+require("heroku/helpers/heroku_postgresql")
+require("#{File.dirname(__FILE__)}/lib/ext/heroku/helpers/heroku_postgresql")
