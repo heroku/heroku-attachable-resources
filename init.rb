9e30c28f81e0522463405f2f8ebae3eac647fd75
David Dollar <ddollar@gmail.com>
2012-09-20 12:24:19 -0400

    dont need to require this

--- init.rb
+++ init.rb
@@ -6,6 +6,3 @@ require("#{File.dirname(__FILE__)}/lib/ext/heroku/command/addons")
 
 require("heroku/command/pg")
 require("#{File.dirname(__FILE__)}/lib/ext/heroku/command/pg")
-
-require("heroku/helpers/heroku_postgresql")
-require("#{File.dirname(__FILE__)}/lib/ext/heroku/helpers/heroku_postgresql")
